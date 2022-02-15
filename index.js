6a4f689ff176a4e6aba21d7b3959c5183d49adae
Tobias König <tobias.koenig@persona-digital.de>
2022-02-15 11:39:55 +0100

    applied sonarqube hints

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let fs = require("fs");
-let serveStatic = require('serve-static');
-let sanitizeOptions = require('./util/options').sanitizeOptions;
-let findEncoding = require('./util/encoding-selection').findEncoding;
+let serveStatic = require("serve-static");
+let sanitizeOptions = require("./util/options").sanitizeOptions;
+let findEncoding = require("./util/encoding-selection").findEncoding;
 let mime = serveStatic.mime;
 
 module.exports = expressStaticGzipMiddleware;
@@ -10,165 +10,176 @@ module.exports = expressStaticGzipMiddleware;
  * Generates a middleware function to serve pre-compressed files. It is build on top of serveStatic.
  * The pre-compressed files need to be placed next to the original files, in the provided `root` directory.
  * @param { string } root: directory to staticly serve files from
- * @param { expressStaticGzip.ExpressStaticGzipOptions } options: options to change module behaviour  
+ * @param { expressStaticGzip.ExpressStaticGzipOptions } options: options to change module behaviour
  * @returns express middleware function
  */
 function expressStaticGzipMiddleware(root, options) {
-    let opts = sanitizeOptions(options);
-    let serveStaticMiddleware = serveStatic(root, opts.serveStatic || null);
-    let compressions = [];
-    let files = {};
+  let opts = sanitizeOptions(options);
+  let serveStaticMiddleware = serveStatic(root, opts.serveStatic || null);
+  let compressions = [];
+  let files = {};
 
-    registerCompressionsFromOptions();
-    parseRootDirForCompressedFiles();
-    
-    return expressStaticGzip;
+  registerCompressionsFromOptions();
+  parseRootDirForCompressedFiles();
 
-    function expressStaticGzip(req, res, next) {
-        changeUrlFromDirectoryToIndexFile(req);
+  return expressStaticGzip;
 
-        let clientsAcceptedEncodings = req.headers["accept-encoding"];
+  function expressStaticGzip(req, res, next) {
+    changeUrlFromDirectoryToIndexFile(req);
 
-        let path = "";
-        try {
-            path = decodeURIComponent(req.path);
-        } catch (e) {
-            res.status(400).send(e.message);
-            return;
-        }
-        
-        let fileWithMatchingPath = files[path];
-        if (fileWithMatchingPath) {
-            // The Vary Header is required for caching proxies to work properly
-            res.setHeader("Vary", "Accept-Encoding");
+    let clientsAcceptedEncodings = req.headers["accept-encoding"];
 
-            let compression = findEncoding(clientsAcceptedEncodings, fileWithMatchingPath.compressions, opts.orderPreference);
-            if (compression) {
-                convertToCompressedRequest(req, res, compression);
-            }
-        }
-
-        serveStaticMiddleware(req, res, next);
+    let path = "";
+    try {
+      path = decodeURIComponent(req.path);
+    } catch (e) {
+      res.status(400).send(e.message);
+      return;
     }
 
-    function registerCompressionsFromOptions() {
-        if (opts.customCompressions && opts.customCompressions.length > 0) {
-            for (var i = 0; i < opts.customCompressions.length; i++) {
-                var customCompression = opts.customCompressions[i];
-                registerCompression(customCompression.encodingName, customCompression.fileExtension);
-            }
-        }
+    let fileWithMatchingPath = files[path];
+    if (fileWithMatchingPath) {
+      // The Vary Header is required for caching proxies to work properly
+      res.setHeader("Vary", "Accept-Encoding");
 
-        if (opts.enableBrotli) {
-            registerCompression("br", "br");
-        }
-
-        registerCompression("gzip", "gz");
+      let compression = findEncoding(
+        clientsAcceptedEncodings,
+        fileWithMatchingPath.compressions,
+        opts.orderPreference
+      );
+      if (compression) {
+        convertToCompressedRequest(req, res, compression);
+      }
     }
 
-    function convertToCompressedRequest(req, res, compression) {
-        var type = mime.lookup(req.path);
-        var charset = mime.charsets.lookup(type);
-        var search = req.url.split('?').splice(1).join('?');
+    serveStaticMiddleware(req, res, next);
+  }
 
-        if (search !== "") {
-            search = "?" + search;
-        }
-
-        req.url = req.path + compression.fileExtension + search;
-        res.setHeader("Content-Encoding", compression.encodingName);
-        res.setHeader("Content-Type", type + (charset ? "; charset=" + charset : ""));
+  function registerCompressionsFromOptions() {
+    if (opts.customCompressions && opts.customCompressions.length > 0) {
+      for (let customCompression of opts.customCompressions) {
+        registerCompression(
+          customCompression.encodingName,
+          customCompression.fileExtension
+        );
+      }
     }
 
-    function changeUrlFromDirectoryToIndexFile(req) {
-        if (opts.index && req.url.endsWith("/")) {
-            req.url += opts.index;
-        }
-    }
-    
-    function parseRootDirForCompressedFiles() {
-        if (compressions.length > 0) {
-            findCompressedFilesInDirectory(root);
-        }
+    if (opts.enableBrotli) {
+      registerCompression("br", "br");
     }
 
-    function findCompressedFilesInDirectory(directoryPath) {
-        if (!fs.existsSync(directoryPath))  return; 
-        
-        var filesInDirectory = fs.readdirSync(directoryPath);
-        for (var i = 0; i < filesInDirectory.length; i++) {
-            var filePath = directoryPath + "/" + filesInDirectory[i];
-            var stats = fs.statSync(filePath);
-            if (stats.isDirectory()) {
-                findCompressedFilesInDirectory(filePath);
-            } else {
-                addMatchingCompressionsToFile(filesInDirectory[i], filePath);
-            }
-        }
+    registerCompression("gzip", "gz");
+  }
+
+  function convertToCompressedRequest(req, res, compression) {
+    let type = mime.lookup(req.path);
+    let charset = mime.charsets.lookup(type);
+    let search = req.url.split("?").splice(1).join("?");
+
+    if (search !== "") {
+      search = "?" + search;
     }
 
-    /**
-     * Takes a filename and checks if there is any compression type matching the file extension.
-     * Adds all matching compressions to the file.     
-     * @param {string} fileName
-     * @param {string} fillFilePath
-     */
-    function addMatchingCompressionsToFile(fileName, fullFilePath) {
-        for (var i = 0; i < compressions.length; i++) {
-            if (fileName.endsWith(compressions[i].fileExtension)) {
-                addCompressionToFile(fullFilePath, compressions[i]);
-                return;
-            }
-        }
+    req.url = req.path + compression.fileExtension + search;
+    res.setHeader("Content-Encoding", compression.encodingName);
+    res.setHeader(
+      "Content-Type",
+      type + (charset ? "; charset=" + charset : "")
+    );
+  }
+
+  function changeUrlFromDirectoryToIndexFile(req) {
+    if (opts.index && req.url.endsWith("/")) {
+      req.url += opts.index;
+    }
+  }
+
+  function parseRootDirForCompressedFiles() {
+    if (compressions.length > 0) {
+      findCompressedFilesInDirectory(root);
+    }
+  }
+
+  function findCompressedFilesInDirectory(directoryPath) {
+    if (!fs.existsSync(directoryPath)) return;
+
+    let filesInDirectory = fs.readdirSync(directoryPath);
+    for (let file of filesInDirectory) {
+      let filePath = directoryPath + "/" + file;
+      let stats = fs.statSync(filePath);
+      if (stats.isDirectory()) {
+        findCompressedFilesInDirectory(filePath);
+      } else {
+        addMatchingCompressionsToFile(file, filePath);
+      }
+    }
+  }
+
+  /**
+   * Takes a filename and checks if there is any compression type matching the file extension.
+   * Adds all matching compressions to the file.
+   * @param {string} fileName
+   * @param {string} fillFilePath
+   */
+  function addMatchingCompressionsToFile(fileName, fullFilePath) {
+    for (let compression of compressions) {
+      if (fileName.endsWith(compression.fileExtension)) {
+        addCompressionToFile(fullFilePath, compression);
+        return;
+      }
+    }
+  }
+
+  /**
+   * Adds the compression to the file's list of available compressions
+   * @param {string} filePath
+   * @param {Compression} compression
+   */
+  function addCompressionToFile(filePath, compression) {
+    let srcFilePath = filePath
+      .replace(root, "")
+      .replace(compression.fileExtension, "");
+    let existingFile = files[srcFilePath];
+    if (!existingFile) {
+      files[srcFilePath] = { compressions: [compression] };
+    } else {
+      existingFile.compressions.push(compression);
+    }
+  }
+
+  /**
+   * Registers a new compression to the module.
+   * @param {string} encodingName
+   * @param {string} fileExtension
+   */
+  function registerCompression(encodingName, fileExtension) {
+    if (!findCompressionByName(encodingName)) {
+      compressions.push(new Compression(encodingName, fileExtension));
+    }
+  }
+
+  /**
+   * @param {string} encodingName
+   * @param {string} fileExtension
+   * @returns {{encodingName:string, fileExtension:string}}
+   */
+  function Compression(encodingName, fileExtension) {
+    this.encodingName = encodingName;
+    this.fileExtension = "." + fileExtension;
+  }
+
+  /**
+   * @param {string} encodingName
+   * @returns {{encodingName:string, fileExtension:string}}
+   */
+  function findCompressionByName(encodingName) {
+    for (let compression of compressions) {
+      if (compression.encodingName === encodingName) {
+        return compression;
+      }
     }
 
-    /**
-     * Adds the compression to the file's list of available compressions
-     * @param {string} filePath
-     * @param {Compression} compression
-     */
-    function addCompressionToFile(filePath, compression) {
-        var srcFilePath = filePath.replace(root, "").replace(compression.fileExtension, "");
-        var existingFile = files[srcFilePath];
-        if (!existingFile) {
-            files[srcFilePath] = { compressions: [compression] };
-        } else {
-            existingFile.compressions.push(compression);
-        }
-    }
-
-    /**
-     * Registers a new compression to the module.
-     * @param {string} encodingName
-     * @param {string} fileExtension
-     */
-    function registerCompression(encodingName, fileExtension) {
-        if (!findCompressionByName(encodingName)) {
-            compressions.push(new Compression(encodingName, fileExtension));
-        }
-    }
-
-    /**
-     * @param {string} encodingName
-     * @param {string} fileExtension
-     * @returns {{encodingName:string, fileExtension:string}}
-     */
-    function Compression(encodingName, fileExtension) {
-        this.encodingName = encodingName;
-        this.fileExtension = "." + fileExtension;
-    }
-
-    /**
-     * @param {string} encodingName
-     * @returns {{encodingName:string, fileExtension:string}}
-     */
-    function findCompressionByName(encodingName) {
-        for (var i = 0; i < compressions.length; i++) {
-            if (compressions[i].encodingName === encodingName) {
-                return compressions[i];
-            }
-        }
-
-        return null;
-    }
+    return null;
+  }
 }
--- util/encoding-selection.js
+++ util/encoding-selection.js
@@ -1,95 +1,107 @@
 // see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Encoding
 
 // Indicates the identity function (i.e. no compression, nor modification)
-const IDENTITY = 'identity';
+const IDENTITY = "identity";
 
 /**
- * 
+ *
  * @param {string} acceptEncoding Content of the accept-encoding header
- * @param {{encodingName: string, fileExtension: string}[]} availableCompressions 
- * @param {{string[]} preference 
+ * @param {{encodingName: string, fileExtension: string}[]} availableCompressions
+ * @param {{string[]} preference
  */
 function findEncoding(acceptEncoding, availableCompressions, preference) {
-    if (acceptEncoding) {
-        let sortedEncodingList = parseEncoding(acceptEncoding);
-        sortedEncodingList = takePreferenceIntoAccount(sortedEncodingList, preference);
-        return findFirstMatchingCompression(sortedEncodingList, availableCompressions);
-    }
+  if (acceptEncoding) {
+    let sortedEncodingList = parseEncoding(acceptEncoding);
+    sortedEncodingList = takePreferenceIntoAccount(
+      sortedEncodingList,
+      preference
+    );
+    return findFirstMatchingCompression(
+      sortedEncodingList,
+      availableCompressions
+    );
+  }
 
-    return null;
+  return null;
 }
 
-function findFirstMatchingCompression(sortedEncodingList, availableCompressions) {
-    for (const encoding of sortedEncodingList) {
-        if (encoding === IDENTITY) {
-            return null;
-        }
-        for (let i = 0; i < availableCompressions.length; i++) {
-            if (encoding === '*' || encoding === availableCompressions[i].encodingName) {
-                return availableCompressions[i];
-            }
-        }
+function findFirstMatchingCompression(
+  sortedEncodingList,
+  availableCompressions
+) {
+  for (const encoding of sortedEncodingList) {
+    if (encoding === IDENTITY) {
+      return null;
     }
-    return null;
+    for (let i = 0; i < availableCompressions.length; i++) {
+      if (
+        encoding === "*" ||
+        encoding === availableCompressions[i].encodingName
+      ) {
+        return availableCompressions[i];
+      }
+    }
+  }
+  return null;
 }
 
 /**
- * 
- * @param {string[]} sortedEncodingList 
- * @param {string[]} preferences 
+ *
+ * @param {string[]} sortedEncodingList
+ * @param {string[]} preferences
  */
 function takePreferenceIntoAccount(sortedEncodingList, preferences) {
-    if (!preferences || preferences.length === 0) {
-        return sortedEncodingList;
-    }
-
-    for (let i = preferences.length; i >= 0; i--) {
-        let pref = preferences[i];
-        let matchIdx = sortedEncodingList.indexOf(pref);
-
-        if (matchIdx >= 0) {
-            sortedEncodingList.splice(matchIdx, 1);
-            sortedEncodingList.splice(0, 0, pref);
-        }
-    }
-
+  if (!preferences || preferences.length === 0) {
     return sortedEncodingList;
+  }
+
+  for (let pref of preferences) {
+    let matchIdx = sortedEncodingList.indexOf(pref);
+
+    if (matchIdx >= 0) {
+      sortedEncodingList.splice(matchIdx, 1);
+      sortedEncodingList.splice(0, 0, pref);
+    }
+  }
+
+  return sortedEncodingList;
 }
 
 /**
- * 
- * @param {string} acceptedEncoding 
+ *
+ * @param {string} acceptedEncoding
  */
 function parseEncoding(acceptedEncoding) {
-    return acceptedEncoding.split(',')
-        .map(encoding => parseQuality(encoding))
-        .sort((encodingA, encodingB) => encodingB.q - encodingA.q)
-        .filter(encoding => encoding.q > 0)
-        .map(encoding => encoding.name);
+  return acceptedEncoding
+    .split(",")
+    .map((encoding) => parseQuality(encoding))
+    .sort((encodingA, encodingB) => encodingB.q - encodingA.q)
+    .filter((encoding) => encoding.q > 0)
+    .map((encoding) => encoding.name);
 }
 
 /**
  * Parses the quality value of an entry. Empty value will be set to 1.
- * @param {string} encoding 
+ * @param {string} encoding
  * @returns {{name: string, q: number}[]}
  */
 function parseQuality(encoding) {
-    let eSplit = encoding.split(';');
-    try {
-        if (eSplit.length > 1) {
-            const num = eSplit[1].trim().match(/q=(.*)/)[1];
-            return {
-                name: eSplit[0].trim(),
-                q: parseFloat(num)
-            };
-        }
-    } catch (ex) { }
-    return {
+  let eSplit = encoding.split(";");
+  try {
+    if (eSplit.length > 1) {
+      const num = eSplit[1].trim().match(/q=(.*)/)[1];
+      return {
         name: eSplit[0].trim(),
-        q: 1
-    };
+        q: parseFloat(num),
+      };
+    }
+  } catch (ex) {}
+  return {
+    name: eSplit[0].trim(),
+    q: 1,
+  };
 }
 
 module.exports = {
-    findEncoding: findEncoding
+  findEncoding: findEncoding,
 };
